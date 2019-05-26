d5f595da04e90fd92c2e13b1178f3393986b1a30
bipsBro <31367252+bipsBro@users.noreply.github.com>
2019-05-26 22:27:29 +0545

    adding success props

--- src/components/input.js
+++ src/components/input.js
@@ -2,16 +2,17 @@ import React from "react"
 import style from "styled-components"
 
 const Input = style.input`
-	padding:10px;
-	margin:10px 0;
-
+    padding:10px;
+    margin:10px 0;
+    /* border-color: ${(props) => props.success ? 'green' : none }; */
+    border-color: ${({success}) => success ? 'green' : none };
 `
 
 
-function Text({ type, placeholder, value, ...props }) {
-	return(
-         <Input type={type} placeholder={placeholder} value={value} {...props} />
-		);
+function Text({ type, placeholder, value, success, ...props }) {
+    return(
+       <Input type={type} placeholder={placeholder} value={value} success={success} {...props} />
+     );
 }
 
 export {Text}
